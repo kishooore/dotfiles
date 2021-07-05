961ce377396dda2521dc3a854f247a6f1f302e27
Kishore Garapati <kgarapati@opentext.com>
2021-07-05 15:59:07 +0530

    modify install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -s ~/dotfiles/launch_jdtls ~/.config/nvim/lsp/java/launch_jdtls
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/tmux-keybindings.conf ~/.tmux/tmux-keybindings.conf
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+source ~/.zshrc
+tmux source ~/.tmux.conf
