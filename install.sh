9ce90f3fc4beb7b2a99185368d889839778b8608
Kishore Garapati <kishoregarapati1992@gmail.com>
2021-07-05 16:14:25 +0530

    fix paths

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
-ln -s ~/dotfiles/lua ~/.config/nvim/lua
+ln -s ~/dotfiles/lua ~/.config/nvim
 ln -s ~/dotfiles/launch_jdtls ~/.config/nvim/lsp/java/launch_jdtls
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/tmux-keybindings.conf ~/.tmux/tmux-keybindings.conf
