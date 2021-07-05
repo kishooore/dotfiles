9554d30edb496a982df603d4290def74a1fdbb1e
Kishore Garapati <kgarapati@opentext.com>
2021-07-05 15:57:38 +0530

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/lua ~/.config/nvim/lua
+ln -s ~/dotfiles/launch_jdtls ~/.config/nvim/lsp/java/launch_jdtls
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux-keybindings.conf ~/.tmux/tmux-keybindings.conf
+ln -s ~/dotfiles/.zshrc ~/.zshrc
