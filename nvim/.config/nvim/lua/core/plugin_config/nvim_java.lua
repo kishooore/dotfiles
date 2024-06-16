57b8db1d86656fd08e05bc49a299114659ab3e5a
kishore <kishore@cloudera.com>
2024-06-17 00:37:09 +0530

    [lsp] add lsp key bindings

--- nvim/.config/nvim/lua/core/plugin_config/nvim_java.lua
+++ nvim/.config/nvim/lua/core/plugin_config/nvim_java.lua
@@ -1,3 +1,25 @@
 require('java').setup()
 
-require('lspconfig').jdtls.setup({})
+function lsp_keymap(bufnr)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require('lspconfig').jdtls.setup({
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    lsp_keymap(bufnr)
+  end
+})
