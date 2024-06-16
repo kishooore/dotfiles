d937c501e4329fab3298c59959532848e6435bbf
kishore <kishore@cloudera.com>
2024-06-16 22:19:02 +0530

    [completion] enable auto completions

--- nvim/.config/nvim/lua/core/plugin_config/init.lua
+++ nvim/.config/nvim/lua/core/plugin_config/init.lua
@@ -1,2 +1,3 @@
 require("core.plugin_config.telescope")
 require("core.plugin_config.nvim_java")
+require("core.plugin_config.completions")
--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -1,26 +1,32 @@
 require("lazy").setup({
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  "rafamadriz/friendly-snippets",
   "tpope/vim-fugitive",
+  "christoomey/vim-tmux-navigator",
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.4",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
   {
-    'nvim-java/nvim-java',
+    "nvim-java/nvim-java",
     dependencies = {
-      'nvim-java/lua-async-await',
-      'nvim-java/nvim-java-refactor',
-      'nvim-java/nvim-java-core',
-      'nvim-java/nvim-java-test',
-      'nvim-java/nvim-java-dap',
-      'MunifTanjim/nui.nvim',
-      'neovim/nvim-lspconfig',
-      'mfussenegger/nvim-dap',
+      "nvim-java/lua-async-await",
+      "nvim-java/nvim-java-refactor",
+      "nvim-java/nvim-java-core",
+      "nvim-java/nvim-java-test",
+      "nvim-java/nvim-java-dap",
+      "MunifTanjim/nui.nvim",
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
       {
-        'williamboman/mason.nvim',
+        "williamboman/mason.nvim",
         opts = {
           registries = {
-            'github:nvim-java/mason-registry',
-            'github:mason-org/mason-registry',
+            "github:nvim-java/mason-registry",
+            "github:mason-org/mason-registry",
           },
         },
       }
